e54e309aee969bdf531dd48b3b76e22969983957
Mohamed Elatabany <mmemo4855@gmail.com>
2020-05-03 02:33:11 +0200

    searchBox

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React from 'react'
- import { robots } from './robots'
+import React, { Component } from 'react'
+import { robots } from './robots'
 
 import Cardlist from './Cardlist'
 import SearchBox from './SearchBox'
 
 
- const App = () => {
- 	return (
-		<div className="tc">
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+    		robots: robots,
+    		searchfield: ''
+		}
+	}
+
+
+
+
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value})
+	};
+
+
+    render() {
+
+    	 const filteredRobots = this.state.robots.filter ( user => {
+			return user.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		})
+
+        return (
+            <div className="tc">
 			<h1> Robo Friends </h1>
-			<SearchBox />
-			<Cardlist robots={robots} />
+			<SearchBox searchChange={this.onSearchChange}/>
+			<Cardlist robots={filteredRobots} />
 		</div>
- 		);
- }
+        );
+    }
+}
 
 
- export default App
+export default App
